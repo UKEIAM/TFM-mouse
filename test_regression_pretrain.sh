9f4a80d30420c0d615929b3978b3532e0463fd20
Bart van der Woude <bartwoude@gmail.com>
2026-06-06 21:49:31 +0000

    Finished notebook for calculating metrics and comparing to Eagle eye baseline.

--- test_regression_pretrain.sh
+++ test_regression_pretrain.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
 python -m tfmplayground.external_priors \
---lib mouse \
---save_path /data/PFN/Mouse/prior_mouse.h5 \
---num_batches 20000 \
---batch_size 5 \
---prior_type mouse \
+--lib tabpfn \
+--save_path /data/PFN/Mouse/Experiments/TabPFN_Mouse1/prior_tabpfn.h5 \
+--num_batches 4000 \
+--batch_size 4 \
+--prior_type prior_bag \
 --min_features 3 \
 --max_features 3 \
---min_seq_len 10 \
---max_seq_len 50 \
+--min_seq_len 500 \
+--max_seq_len 2500 \
 --max_classes 0
+
+python pretrain_regression.py \
+--epochs 80 \
+--steps 400 \
+--batchsize 2 \
+--accumulate 2 \
+--priordump /data/PFN/Mouse/Experiments/TabPFN_Mouse1/prior_tabpfn.h5 \
+--saveweights /data/PFN/Mouse/Experiments/TabPFN_Mouse1/pretrained_tabpfn.pth \
+--savebuckets /data/PFN/Mouse/Experiments/TabPFN_Mouse1/buckets_tabpfn.pth
+
+# python -m tfmplayground.external_priors \
+# --lib mouse \
+# --save_path /data/PFN/Mouse/Experiments/TabPFN_Mouse1/prior_mouse.h5 \
+# --num_batches 20000 \
+# --batch_size 5 \
+# --prior_type mouse \
+# --min_features 3 \
+# --max_features 3 \
+# --min_seq_len 10 \
+# --max_seq_len 50 \
+# --max_classes 0
 
 python pretrain_regression.py \
 --epochs 100 \
 --steps 5000 \
 --batchsize 1 \
 --accumulate 25 \
---priordump /data/PFN/Mouse/prior_mouse.h5 \
---saveweights /data/PFN/Mouse/pretrained_mousepfn.pth \
---savebuckets /data/PFN/Mouse/buckets_mousepfn.pth
-# --loadcheckpoint /data/PFN/Mouse/pretrained_tabpfn.pth
-
-# python -m tfmplayground.external_priors \
-# --lib tabpfn \
-# --save_path /data/PFN/Mouse/prior_tabpfn.h5 \
-# --num_batches 4000 \
-# --batch_size 4 \
-# --prior_type prior_bag \
-# --min_features 3 \
-# --max_features 3 \
-# --min_seq_len 500 \
-# --max_seq_len 2500 \
-# --max_classes 0
-
-# python pretrain_regression.py \
-# --epochs 80 \
-# --steps 400 \
-# --batchsize 2 \
-# --accumulate 2 \
-# --priordump prior_tabpfn.h5 \
-# --saveweights /data/PFN/Mouse/pretrained_tabpfn.pth \
-# --savebuckets /data/PFN/Mouse/buckets_tabpfn.pth
+--priordump /data/PFN/Mouse/Experiments/TabPFN_Mouse1/prior_mouse.h5 \
+--saveweights /data/PFN/Mouse/Experiments/TabPFN_Mouse1/pretrained_mousepfn.pth \
+--savebuckets /data/PFN/Mouse/Experiments/TabPFN_Mouse1/buckets_mousepfn.pth \
+--loadcheckpoint /data/PFN/Mouse/Experiments/TabPFN_Mouse1/pretrained_tabpfn.pth
